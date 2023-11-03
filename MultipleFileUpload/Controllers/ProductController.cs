28be70d15deb40bbd2050194afd68355918b104a
mikepalie <michalis.palierakis@gmail.com>
2023-11-03 13:37:52 +0200

    Add project files.

---
+++ MultipleFileUpload/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using MultipleFileUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleFileUpload.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Index", new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Save(Product product, HttpPostedFileBase[] photos)
+        {
+            if (photos != null)
+            {
+                List<string> files = new List<string>();
+                foreach (var photo in photos)
+                {
+                    
+                    photo.SaveAs(Server.MapPath("~/Content/Images/" + photo.FileName));
+                    files.Add(photo.FileName);
+                }
+                product.Photos = files;
+            }
+            ViewBag.product = product;
+            return View("Success");
+        }
+
+
+
+
+
+    }
+}
---
+++ MultipleFileUpload/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultipleFileUpload.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public List<string> Photos { get; set; }
+    }
+}
